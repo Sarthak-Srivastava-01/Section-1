7f4be0663c6ea1d9d61b2d2302e3d8beab837737
Sarthak-Srivastava-01 <sarthak.srivastava1398@gmail.com>
2024-08-28 11:14:06 +0530

    conditions

---
+++ Javascript/Datatypes.js
@@ -0,0 +1,26 @@
+// There are two types of datatypes
+// 1. Primitive
+// - Number
+// - String
+// - Boolean
+// - null
+// - undefined
+
+// 2. Non-Primitive
+// - Array
+// - Object
+
+let a = 34
+let b = 12
+sum = a + b
+console.log(sum)
+console.log(typeof sum)
+
+let name = "Sarthak"
+console.log(name)
+console.log(typeof name)
+
+// Area of Circle
+let pi = 3.14159265358979323846;
+let r = 5
+console.log(pi * r * r)
---
+++ Javascript/conditions.js
@@ -0,0 +1,41 @@
+// For single conditions
+let age = 23;
+if (age >= 18) {
+    console.log("You are eligible to vote")
+} else {
+    console.log("Grow Up First")
+}
+
+console.log("\n")
+
+// For multiple cconditions
+let marks = 89;
+if (marks >= 90) {
+    console.log("A+")
+} else if (marks >= 80) {
+    console.log("A")
+} else if (marks >= 70) {
+    console.log("B")
+} else if (marks >= 60) {
+    console.log("C")
+} else {
+    console.log("fail")
+}
+
+console.log("\n")
+
+let letter = "A"
+if (letter == "A" || letter == "E" || letter == "I" || letter == "O" || letter == "U" || letter == "a" || letter == "e" || letter == "i" || letter == "o" || letter == "u") {
+    console.log("It's a Vowel")
+} else {
+    console.log("Not a Vowel")
+}
+
+console.log("\n")
+
+let num2 = 20
+if (num2 % 2 == 0) {
+    console.log("It's Even")
+} else {
+    console.log("It's Odd")
+}
